ce6bbec62f63125b9c7068da78ba4a79023053c0
josegabrielfc <jgfchona@gmail.com>
2024-01-21 00:53:18 -0500

    fix something

--- src/App.java
+++ src/App.java
@@ -1,7 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         BlackJack blackJack = new BlackJack();
-        int x = blackJack.boardHeight;
-        System.out.println(x);
     }
 }
